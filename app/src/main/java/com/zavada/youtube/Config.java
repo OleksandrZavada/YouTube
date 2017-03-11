b5428d9e1b5895c99ba3563ecbdf1716fa13bcd7
Zavada <oleksandr.zavada@gmail.com>
2017-03-11 21:57:04 +0200

    Initial commit

---
+++ app/src/main/java/com/zavada/youtube/Config.java
@@ -0,0 +1,14 @@
+package com.zavada.youtube;
+
+/**
+ * Created by zavada on 11.03.17.
+ */
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyDxU18_-X1PytgbRviVchnM-t7nZ0D2NxM";
+
+}
